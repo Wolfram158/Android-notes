4f2053a780b066dea476b55240b3b8568a785830
Lubnin_Ilia <wolframalpha7134@gmail.com>
2024-08-23 20:39:37 +0300

    java.lang.IllegalArgumentException: Parcel: unknown type for value Offset(207.0, 247.0)

--- Jetpack Compose playground/JetpackComposePlayground/app/src/main/java/android/learn/jetpackComposePlayground/examples4/TableState.kt
+++ Jetpack Compose playground/JetpackComposePlayground/app/src/main/java/android/learn/jetpackComposePlayground/examples4/TableState.kt
@@ -21,20 +21,20 @@ data class TableState(
             save = {
                 val table = it.value
                 listOf(
-                    table.leftCorners,
-                    table.centers,
+                    table.leftCorners.map { it1 -> Pair(it1.x, it1.y) },
+                    table.centers.map { it1 -> Pair(it1.x, it1.y) },
                     table.isCross,
                     table.gameState,
                     table.summary
                 )
             },
-            restore = {
+            restore = { it1 ->
                 val table = TableState(
-                    leftCorners = it[0] as List<Offset>,
-                    centers = it[1] as List<Offset>,
-                    isCross = it[2] as Boolean,
-                    gameState = it[3] as GameState,
-                    summary = it[4] as List<List<CellState>>
+                    leftCorners = (it1[0] as List<Pair<Float, Float>>).map { Offset(it.first, it.second) },
+                    centers = (it1[1] as List<Pair<Float, Float>>).map { Offset(it.first, it.second) },
+                    isCross = it1[2] as Boolean,
+                    gameState = it1[3] as GameState,
+                    summary = it1[4] as List<List<CellState>>
                 )
                 mutableStateOf(table)
             }
